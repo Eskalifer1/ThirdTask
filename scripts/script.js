443738663c8b8a23094426284f7aecd6cda914bd
Artem Achkasov <artemkryt1800@gmail.com>
2022-11-20 18:38:16 +0200

    swiper was fixed

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,36 @@ const swiper = new Swiper(".swiper", {
     touchRatio: 0.25,
     speed:800,
     spaceBetween: 30,
-    allowSlidePrev: false,
+    // allowSlidePrev: false,
     slidesOffsetAfter: 20,
-    slidesOffsetBefore: 10
+    slidesOffsetBefore: 10,
+    breakpoints: {
+        1090:{
+            slidesPerView:5
+        },
+        890:{
+            slidesPerView: 4.5
+        },
+        780:{
+            slidesPerView: 4
+        },
+        690:{
+            slidesPerView: 3.5
+        },
+        595:{
+            slidesPerView: 3
+        },
+        490:{
+            slidesPerView: 2.5
+        },
+        400:{
+            slidesPerView: 2
+        },
+        360:{
+            slidesPerView: 1.5
+        },
+        320:{
+            slidesPerView: 1
+        }
+    }
 })
